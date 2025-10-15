8ddece3b93a3e537f234b5140dbb2bf27a83c34e
Ygor.Rocha <ygorrocha757@gmail.com>
2025-10-14 21:39:32 -0300

    fix: adicionando novas routes

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,20 @@ use App\Http\Controllers\{
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/cadastrar', [UserController::class, 'create']);
+
+Route::post('/cadastrar', [UserController::class, 'create'])->middleware(['auth:sanctum', 'can:create,App\Models\User']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::prefix('user')->group(function() {
         Route::get('/', [UserController::class, 'index']);
+        
+        Route::get('/list', [UserController::class, 'list'])->middleware('can:viewAny,App\Models\User'); // Admin lista todos
+        Route::get('/{id}', [UserController::class, 'show'])->middleware('can:view,user');
+        Route::put('/{id}', [UserController::class, 'update'])->middleware('can:update,user');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->middleware('can:delete,user'); // Apenas Admin
     });
 
     Route::prefix('estado')->group(function () {
@@ -31,11 +38,14 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::prefix('endereco')->group(function () {
+
         Route::post('/', [EnderecoController::class, 'create']);
     });
 
     Route::prefix('condominio')->group(function() {
+
         Route::post('/', [CondominioController::class, 'create']);
+
         Route::get('/', [CondominioController::class, 'list']);
         Route::get('/buscar', [CondominioController::class, 'search']);
 
@@ -44,9 +54,12 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::get('/', [BlocoController::class, 'list']);
 
             Route::prefix('apartamento')->group(function(){
+
                 Route::post('/', [ApartamentoController::class, 'create']);
                 Route::get('/', [ApartamentoController::class, 'list']);
-                Route::put('/atualizar/{uuid}', [ApartamentoController::class, 'update']);
+
+                Route::put('/atualizar/{uuid}', [ApartamentoController::class, 'update'])->middleware('can:update,apartamento');
+                Route::delete('/{uuid}', [ApartamentoController::class, 'destroy'])->middleware('can:delete,apartamento');
             });
         });
     });
